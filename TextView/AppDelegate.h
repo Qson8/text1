f7c98ac414be33c03ce9d3896960e26f605c7f26
Qson <qiubite2013@126.com>
2017-07-19 11:07:52 +0800

    提交

---
+++ TextView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextView
+//
+//  Created by Qson on 2017/7/19.
+//  Copyright © 2017年 Qson11. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
